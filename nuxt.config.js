a8486a9ed1d3bf5687416391b10d52032f1f907e
Symond Wang <wzc5840@gmail.com>
2020-05-25 14:05:33 +0900

    Remove ant-design-vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@/plugins/antd-ui'],
+  plugins: [],
   /*
    ** Nuxt.js dev-modules
    */
@@ -59,4 +59,33 @@ module.exports = {
     extend(config, ctx) {}
   },
   dev: process.env.NODE_ENV !== 'production'
+}
+module.exports = {
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: '...' },
+      { hid: 'description', name: 'description', content: '...' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png?v=3' },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href:
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css'
+      }
+    ],
+    script: [
+      {
+        src:
+          'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js'
+      },
+      { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
+      {
+        src:
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js'
+      }
+    ]
+  }
 }
